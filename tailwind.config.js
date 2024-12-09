584fac955863db0817b33de2f1d38fceecb33c65
Edith <eddith@proton.me>
2024-12-10 00:01:04 +0100

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,svelte,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  safelist: [
+    'col-start-1', 'col-start-2', 'col-start-3', 'col-start-4', 'col-start-5', 'col-start-6',
+    'row-start-1', 'row-start-2', 'row-start-3', 'row-start-4', 'row-start-5', 'row-start-6', 'row-start-7', 'row-start-8',
+    'row-start-9', 'row-start-10',
+    'row-start-11', 'row-start-12', 'row-start-13', 'row-start-14', 'row-start-15'
+  ]
+}
+

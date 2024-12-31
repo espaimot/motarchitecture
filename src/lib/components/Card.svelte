8b5784e7d47a60d825d75334851fb9d896fb4547
Edith <eddith@proton.me>
2024-12-31 13:04:18 +0100

    card refactor for ux/ui experience

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -5,50 +5,26 @@
 </script>
 
 <div class="card">
-	<div class="card-inner">
-		<div class="card-front">
-			<img src={card.images[0].src} alt={card.images[0].alt} />
-		</div>
-		<div class="card-back">
-			<img src={card.images[0].src} alt={card.images[0].alt} />
-			<div class="card-overlay">
-				<h3>{card.title}</h3>
-				<p>{card.subtitle}</p>
-			</div>
+	<img src={card.images[0].src} alt={card.images[0].alt} class="card-image" />
+	<div class="card-overlay">
+		<div class="card-text">
+			<h3 class="mb-2 text-xs font-bold md:mb-4 md:text-xl">{card.title}</h3>
+			<p class="text-xs font-medium md:text-sm md:leading-6">{card.subtitle}</p>
 		</div>
 	</div>
 </div>
 
 <style>
 	.card {
-		perspective: 1000px;
-	}
-
-	.card-inner {
 		position: relative;
-		transition: transform 0.6s;
-		transform-style: preserve-3d;
+		overflow: hidden;
+		text-overflow: ellipsis;
 	}
 
-	.card:hover .card-inner {
-		transform: rotateY(180deg);
-		cursor: pointer;
-	}
-
-	.card-front,
-	.card-back {
-		position: absolute;
+	.card-image {
 		width: 100%;
 		height: 100%;
-		backface-visibility: hidden;
-	}
-
-	.card-back {
-		transform: rotateY(180deg);
-	}
-
-	.card-back img {
-		filter: invert(1);
+		object-fit: contain;
 	}
 
 	.card-overlay {
@@ -57,5 +33,26 @@
 		left: 0;
 		width: 100%;
 		height: 100%;
+		background-color: rgba(255, 255, 255, 0.7);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		opacity: 0;
+		transition: opacity 0.3s ease-in-out;
+	}
+
+	.card:hover .card-overlay {
+		opacity: 1;
+		cursor: pointer;
+	}
+
+	.card-text {
+		text-align: justify;
+		color: black;
+		padding: 1rem;
+
+		h3 {
+			text-align: center;
+		}
 	}
 </style>

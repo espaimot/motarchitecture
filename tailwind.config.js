f73b9121320e5e0184e6314feb1960d10b318ef8
Edith <eddith@proton.me>
2024-12-31 13:02:43 +0100

    add tailwind safelist and typo html

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,8 +28,11 @@ export default {
   plugins: [],
   safelist: [
     'col-start-1', 'col-start-2', 'col-start-3', 'col-start-4', 'col-start-5', 'col-start-6',
+    'col-start-7', 'col-start-8', 'col-start-9', 'col-start-10', 'col-start-11', 'col-start-12', 'col-start-13', 'col-start-14',
+    'col-start-15', 'col-start-16', 'col-start-17', 'col-start-18', 'col-start-19', 'col-start-20', 'col-start-21', 'col-start-22',
     'row-start-1', 'row-start-2', 'row-start-3', 'row-start-4', 'row-start-5', 'row-start-6', 'row-start-7', 'row-start-8',
-    'row-start-9', 'row-start-10', 'row-start-11', 'row-start-12', 'row-start-13', 'row-start-14', 'row-start-15',
+    'row-start-9', 'row-start-10', 'row-start-11', 'row-start-12', 'row-start-13', 'row-start-14', 'row-start-15', 'row-start-16',
+    'row-start-17', 'row-start-18', 'row-start-19', 'row-start-20', 'row-start-21', 'row-start-22', 'row-start-23', 'row-start-24',
     'col-span-1', 'col-span-2', 'col-span-3'
   ]
 }

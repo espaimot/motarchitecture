ca1a55b515f226268fff6e0d60a7f56597bc1487
Edith <eddith@proton.me>
2025-01-22 19:39:21 +0100

    modal on card

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,17 +1,46 @@
 <script lang="ts">
+	import { marked } from 'marked';
+	import DOMPurify from 'isomorphic-dompurify';
 	import type { ProjectCard } from '../../types/Card';
 
 	export let card: ProjectCard;
+	let isExpanded = false;
+
+	function toggleExpand(event: Event) {
+		event.stopPropagation();
+		isExpanded = true;
+	}
+
+	function closeExpand(event: Event) {
+		event.stopPropagation();
+		isExpanded = false;
+	}
 </script>
 
-<div class="card">
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<div class={`card ${isExpanded ? 'expanded' : ''}`} on:click={(event) => toggleExpand(event)}>
 	<img src={card.images[0].src} alt={card.images[0].alt} class="card-image" />
-	<div class="card-overlay">
+	<div
+		class="card-overlay flex flex-col items-start justify-start md:flex-row md:items-center md:justify-center md:gap-8 md:p-8"
+	>
+		{#if isExpanded}
+			<img
+				src={card.images[0].src}
+				alt={card.images[0].alt}
+				class="h-1/2 w-full md:h-auto md:w-1/2"
+			/>
+		{/if}
 		<div class="card-text">
 			<h3 class="mb-2 text-xs font-bold md:mb-4 md:text-xl">{card.title}</h3>
-			<p class="text-xs font-medium md:text-sm md:leading-6">{card.subtitle}</p>
+			<p class="text-xs font-medium md:text-sm md:leading-6">
+				{@html DOMPurify.sanitize(marked(card.subtitle, { async: false }))}
+			</p>
 		</div>
 	</div>
+	{#if isExpanded}
+		<button type="button" class="close-button" on:click={(event) => closeExpand(event)}>X</button>
+	{/if}
 </div>
 
 <style>
@@ -19,6 +48,7 @@
 		position: relative;
 		overflow: hidden;
 		text-overflow: ellipsis;
+		cursor: pointer;
 	}
 
 	.card-image {
@@ -54,5 +84,31 @@
 		h3 {
 			text-align: center;
 		}
+	}
+
+	.card.expanded {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		z-index: 1000;
+		background-color: white;
+	}
+
+	.card.expanded .card-overlay {
+		opacity: 1;
+		background-color: rgba(255, 255, 255, 0.9);
+	}
+
+	.close-button {
+		position: absolute;
+		top: 10px;
+		right: 25px;
+		background: none;
+		border: none;
+		font-size: 24px;
+		cursor: pointer;
+		z-index: 1001;
 	}
 </style>
